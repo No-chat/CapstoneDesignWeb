1815b8e44f0b8ecd8ffdbd7e66b6733e1bbc3110
Sungwook Kang <kpuppy110794@gmail.com>
2022-08-23 17:42:27 +0900

    [Test] Add save data test code

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import cors from "cors";
 
 import trafficRouter from './router/traffic.js';
 import config from "./config/localMongo.js";
+import CarValue from "./model/model.js";
 const TRAFFIC_URI = '/api/traffic-infos';
 const app = express();
 
@@ -22,6 +23,15 @@ app.use(TRAFFIC_URI, trafficRouter)
 app.get('/', (req, res, next) => {
   res.status(200);
   res.send("Hello");
+})
+
+app.post('/test', (req, res, next) => {
+  console.log(req.body)
+  const saveData = new CarValue(req.body)
+  console.log(saveData)
+  saveData.save()
+  .then(() => {return res.status(200).json({success: true})})
+  .catch((e) => {return res.status(400).json({success:false,e})})
 })
 
 app.listen(8080);
