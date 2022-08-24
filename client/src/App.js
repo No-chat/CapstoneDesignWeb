f459f0ea2d2906c07133b3b159211d510041bcf0
Sungwook Kang <kpuppy110794@gmail.com>
2022-08-24 14:37:35 +0900

    [Chore] Apply reset.css to App.js & index.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,26 @@
+import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
+// Styling
+import "react-datepicker/dist/react-datepicker.css";
+import "./styles/reset.css"
+
+// Components
+import MainPage from './components/MainPage';
 
 
 
 function App() {
+
+  
   return (
     <div className="App">
-      <p>HELLO React</p>
-
+      <Router>
+        <Routes>
+          <Route exact path='/' element={<MainPage />} />
+          <Route exact path='/filteredData' element={<MainPage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
